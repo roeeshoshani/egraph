c40214aed9543e9b471c22bb8a31cd441ff3fe67
Roee Shoshani <roysho10@gmail.com>
2025-10-03 17:45:22 +0300

    add a test for flattening the union find tree

--- src/union_find.rs
+++ src/union_find.rs
@@ -204,6 +204,9 @@ impl<T> UnionFind<T> {
         }
         self.root_of_item(item_a) == self.root_of_item(item_b)
     }
+    pub fn flatten(&mut self) {
+        todo!()
+    }
 }
 
 #[cfg(test)]
@@ -430,5 +433,54 @@ mod tests {
         for (item, val) in all_items.iter().copied().zip(all_vals.iter().copied()) {
             assert_eq!(union_find[item], val)
         }
+    }
+
+    #[test]
+    fn test_flatten() {
+        let mut union_find = UnionFind::new();
+
+        let a = union_find.create_new_item(());
+        let b = union_find.create_new_item(());
+        let c = union_find.create_new_item(());
+        let d = union_find.create_new_item(());
+        let e = union_find.create_new_item(());
+        let f = union_find.create_new_item(());
+        let g = union_find.create_new_item(());
+
+        let all_items = [a, b, c, d, e, f, g];
+
+        union_find.union(a, b);
+        union_find.union(c, d);
+        union_find.union(e, f);
+        union_find.union(e, g);
+
+        union_find.union(b, g);
+
+        // sanity check the groups
+        chk_groups(&union_find, &all_items, &[&[a, b, e, f, g], &[c, d]]);
+
+        // at this point, some items have different parents even though they are equal
+        assert!(union_find.are_eq(a, f));
+        assert_ne!(
+            union_find.get_parent_of_item(a).unwrap(),
+            union_find.get_parent_of_item(f).unwrap()
+        );
+
+        union_find.flatten();
+
+        // now the equal items should have the same parent
+        assert_eq!(
+            union_find.get_parent_of_item(a).unwrap(),
+            union_find.get_parent_of_item(f).unwrap()
+        );
+
+        // but items that are not equal should still have different parents
+        assert_ne!(
+            union_find.get_parent_of_item(a).unwrap(),
+            union_find.get_parent_of_item(c).unwrap()
+        );
+
+        // make sure that flatenning didn't mess with the groups
+        chk_groups(&union_find, &all_items, &[&[a, b, e, f, g], &[c, d]]);
     }
 }
