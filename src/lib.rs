ef80ae247cf22b2a89a038fdbc7601cdef7c114b
Roee Shoshani <roysho10@gmail.com>
2025-10-03 19:35:51 +0300

    minor rename

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 mod egraph;
 pub mod graph;
-mod matching;
 mod node;
 mod rec_node;
+mod rewrite_rules;
 pub mod union_find;
 mod utils;
 
 pub use egraph::*;
-pub use matching::*;
 pub use node::*;
 pub use rec_node::*;
+pub use rewrite_rules::*;
