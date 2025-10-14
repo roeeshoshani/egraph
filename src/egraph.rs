e78a170d810687664a5ccebd528140e2b111b084
Roee Shoshani <roysho10@gmail.com>
2025-10-15 01:05:55 +0300

    fix tests

--- src/egraph.rs
+++ src/egraph.rs
@@ -1287,25 +1287,15 @@ mod tests {
             .add_enode(
                 BinOp {
                     kind: BinOpKind::Add,
-                    lhs: var0,
-                    rhs: var1,
-                }
-                .into(),
-            )
-            .eclass_id;
-        let bin_op2 = egraph
-            .add_enode(
-                BinOp {
-                    kind: BinOpKind::Add,
                     lhs: un_op_var1,
-                    rhs: un_op_var0,
+                    rhs: var0,
                 }
                 .into(),
             )
             .eclass_id;
 
         // sanity
-        assert!(!egraph.are_eq(bin_op1, bin_op2));
+        assert!(!egraph.are_eq(bin_op0, bin_op1));
 
         let union_res = egraph.union(var0, var1);
         assert_eq!(union_res, UnionRes::New);
@@ -1313,7 +1303,5 @@ mod tests {
 
         assert!(egraph.are_eq(un_op_var0, un_op_var1));
         assert!(egraph.are_eq(bin_op0, bin_op1));
-        assert!(egraph.are_eq(bin_op1, bin_op2));
-        assert!(egraph.are_eq(bin_op0, bin_op2));
     }
 }
