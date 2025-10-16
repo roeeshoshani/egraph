1bd7aa55bf41769a223054a0304369ac115650d6
Roee Shoshani <roysho10@gmail.com>
2025-10-16 23:25:05 +0300

    clean up recursive node code

--- src/rec_node.rs
+++ src/rec_node.rs
@@ -2,15 +2,35 @@ use std::fmt::Display;
 
 use crate::node::{BinOp, GenericNode, UnOp};
 
+/// the link of a recursive node.
 pub enum RecNodeLink {
+    /// a regular recursive node link. just points to another node.
     Regular(Box<RecNode>),
+
+    /// a special link value used to mark a link that loops to a parent node.
+    ///
+    /// loops are not representable in recursive nodes, since each node must have exactly one parent.
+    ///
+    /// but, in some cases, we want to convert some graph representation to a recursive node, and have a representation that
+    /// still encodes all information that could be encoded, but excluding the loops, which obviously can't be encoded in recursive
+    /// nodes.
+    ///
+    /// so, this variant is used to mark such unrepresentable link, while still allowing us to represent the rest of the graph's
+    /// structure in a convenient recursive way.
+    // TODO: maybe we should just avoid using recursive nodes in those scenarios, and instead use graphs, because this is very hacky.
     Loop,
 }
 
+/// a recursive node, which is a node whose link value directly owns the linked-to node.
 pub type RecNode = GenericNode<RecNodeLink>;
+
+/// a bin op which uses recursive nodes.
 pub type RecBinOp = BinOp<RecNodeLink>;
+
+/// a un op which uses recursive nodes.
 pub type RecUnOp = UnOp<RecNodeLink>;
 
+// convenience, convert a recursive node value directly to a recursive link value.
 impl<T> From<T> for RecNodeLink
 where
     RecNode: From<T>,
