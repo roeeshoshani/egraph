34c4ca9a1adf81ee69572f490c97b13f86c5a104
Roee Shoshani <roysho10@gmail.com>
2025-10-19 17:17:43 +0300

    implement distribute over phi rewrite

--- examples/loop.rs
+++ examples/loop.rs
@@ -69,10 +69,84 @@ fn main() {
     let root_eclass = translation_map[root_id];
 
     let rule_set = rewrites![
-        // a + (b + c) => (a + b) + c
-        TemplateRewrite::bin_op_associativity(BinOpKind::IntAdd).build(),
         // a + b => b + a
         TemplateRewrite::bin_op_commutativity(BinOpKind::IntAdd).build(),
+        // x * (phi(initial_term, add(loop_increment, <parent phi>)))
+        // =>
+        // phi(initial_term * x, add(loop_increment * x, <parent phi>))
+        TemplateRewrite {
+            query: TemplateBinOp {
+                kind: BinOpKind::IntMul,
+                lhs: {
+                    // multiplier (aka `x`)
+                    TemplateVar::new(1).into()
+                },
+                rhs: TemplateLink::SpecificBind(
+                    TemplateVar::new(2),
+                    Box::new(
+                        PhiNode {
+                            inputs: vec![
+                                // initial term
+                                TemplateVar::new(3).into(),
+                                // loop body
+                                TemplateBinOp {
+                                    kind: BinOpKind::IntAdd,
+                                    lhs: {
+                                        // loop increment
+                                        TemplateVar::new(4).into()
+                                    },
+                                    rhs: {
+                                        // point back to parent phi
+                                        TemplateVar::new(2).into()
+                                    }
+                                }
+                                .into()
+                            ],
+                            region_node: TemplateVar::new(5).into(),
+                        }
+                        .into()
+                    )
+                ),
+            }
+            .into(),
+            rewrite: TemplateLink::SpecificBind(
+                TemplateVar::new(6),
+                Box::new(
+                    PhiNode {
+                        inputs: vec![
+                            // multiplied initial term
+                            TemplateBinOp {
+                                kind: BinOpKind::IntMul,
+                                lhs: TemplateVar::new(3).into(),
+                                rhs: TemplateVar::new(1).into(),
+                            }
+                            .into(),
+                            // loop body
+                            TemplateBinOp {
+                                kind: BinOpKind::IntAdd,
+                                lhs: {
+                                    // multiplied loop increment
+                                    TemplateBinOp {
+                                        kind: BinOpKind::IntMul,
+                                        lhs: TemplateVar::new(4).into(),
+                                        rhs: TemplateVar::new(1).into(),
+                                    }
+                                    .into()
+                                },
+                                rhs: {
+                                    // point back to new parent phi
+                                    TemplateVar::new(6).into()
+                                }
+                            }
+                            .into()
+                        ],
+                        region_node: TemplateVar::new(5).into()
+                    }
+                    .into()
+                )
+            ),
+        }
+        .build(),
         BinOpConstFoldRewrite,
     ];
 
